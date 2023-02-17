043eabf92747967c3ffbcfd6d1b8a0f610cb5f75
Princewill Samuel Chimdi <princesamuelchukwu@gmail.com>
2023-02-17 08:50:40 -0800

    C program to print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <errno.h>
+
+/**
+ * main - c program to print using perror
+ *
+ * Return: 1
+*/
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
