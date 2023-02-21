0069328b557d9bd414f57d498a8c03474e2ffe27
Princewill Samuel Chimdi <princesamuelchukwu@gmail.com>
2023-02-21 15:46:30 -0800

    Hours of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.c"
+
+/**
+ * jack_bauer - print every minutes of jack bauer
+ * Return: ...
+ */
+
+void jack_bauer(void)
+{
+	int hours = 0;
+	int min = 0;
+
+	while (hours < 24)
+	{
+		while (min < 60)
+		{
+			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n');
+			min++;
+		}
+		min = 0;
+		hours++;
+	}
+}
