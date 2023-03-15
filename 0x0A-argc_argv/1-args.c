b3042de0036cbc4d5f0f9bc014d58724bd1c2012
Princewill Samuel Chimdi <princesamuelchukwu@gmail.com>
2023-03-15 04:16:21 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,11 +4,11 @@
  * main - Aprogram that prints the number of arguement you passed into it
  * @argc: arguement count
  * @argv: arguement vector
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	if (argv[0])
-		printf("%d\n", argc -1);
+		printf("%d\n", argc - 1);
 	return (0);
 }
