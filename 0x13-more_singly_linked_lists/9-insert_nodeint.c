5f51cc31c1c526cdf655660590b2abc18b5653f0
Princewill Samuel Chimdi <princesamuelchukwu@gmail.com>
2023-04-04 20:29:10 -0700

    function that delete a node in a linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,31 +11,27 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int j;
-	listint_t *gent;
-	listint_t *temp = *head;
+	listint_t *gent, *temp = *head;
 
 	gent = malloc(sizeof(listint_t));
-	if (!gent || head)
+	if (gent == NULL)
 		return (NULL);
 	gent->n = n;
-	gent->next = NULL;
+
 	if (idx == 0)
 	{
-		gent->next = *head;
+		gent->next = temp;
 		*head = gent;
 		return (gent);
-
 	}
-	for (j = 0; temp && j < idx; j++)
-	if (j == idx - 1)
+	for (j = 0; j < (idx - 1); j++)
 	{
+		if (temp == NULL || temp->next == NULL)
+			return (NULL);
+		temp = temp->next;
+	}
 		gent->next = temp->next;
 		temp->next = gent;
+
 		return (gent);
-	}
-	else
-		temp = temp->next;
-	{
-		return (NULL);
-	}
 }
